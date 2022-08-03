bccb7dc57d7727e5ef1158ba62e3103c6018720a
Brent <jbmsharpe@gmail.com>
2022-08-03 13:45:19 -0400

    modified funcitons to append the rows

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const container = document.getElementById("container");
 
-function makesGrid() {
-  makesRows();
-  makesColumns();
+function makeGrid() {
+  makeRows(16);
 }
 
-function makesRows(rowNum) {
+function makeRows(rowNum) {
   for (let i = 1; i < rowNum; i++) {
-    container.appendChild(cell);
+    const row = document.createElement("div");
+    row.classList.add("row");
+    makeColumns(16, row);
+    container.appendChild(row);
+    console.log("makeColumns" + i);
   }
 }
 
-function makesColumns(rowNum) {
+function makeColumns(rowNum, row) {
   for (let i = 1; i < rowNum; i++) {
-    container.appendChild(cell);
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    row.appendChild(cell);
+    console.log("makeColumns" + i);
   }
 }
+
+makeGrid();
